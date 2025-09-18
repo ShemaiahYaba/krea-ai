582ae7dfe7bd8718d15e987e10788280c92f4be6
Shemaiah Yaba-Shiaka <apostleshem17@gmail.com>
2025-09-18 20:31:47 +0000

    on smaller widths, make it 3 columns but on larger screens, keep the fou

--- src/components/model-explorer.tsx
+++ src/components/model-explorer.tsx
@@ -33,7 +33,7 @@ export function ModelExplorer() {
             </Button>
         )}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {itemsToShow.map((model) => (
           <ModelCard
             key={model.id}
